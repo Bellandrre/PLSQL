5d706a83c195623b7b165306c1b82c196a47678f
Karthik Kolathumani <32917045+KarthikKola29@users.noreply.github.com>
2021-07-27 14:40:00 -0700

    Update CorrelatedMaxAggreagte.sql

--- CorrelatedMaxAggreagte.sql
+++ CorrelatedMaxAggreagte.sql
@@ -1,5 +1,6 @@
 -- In this PLSQL Statement, I am trying to correlate an aggregated column with another column.
--- Lets say you aggregate by max(x) column and you need corresponding y for that max(x). Thats whats going on!!
+-- Lets say you aggregate by max(x) column and you need corresponding y for that max(x).
+-- Math invloved is just a requirement.
 select FLOOR(TO_NUMBER(MinDispLineNumber)) || DECODE(((MinDispLineNumber -  FLOOR(TO_NUMBER(MinDispLineNumber)) ) * 100000), 0, '' , '.' || ((MinDispLineNumber -  FLOOR(TO_NUMBER(MinDispLineNumber)) ) * 100000) ) as MinDispLineNumber,
        FLOOR(TO_NUMBER(MaxDispLineNumber)) || DECODE(((MaxDispLineNumber -  FLOOR(TO_NUMBER(MaxDispLineNumber)) ) * 100000), 0, '' , '.' || ((MaxDispLineNumber -  FLOOR(TO_NUMBER(MaxDispLineNumber)) ) * 100000) ) as MaxDispLineNumber,
        TotalDispLineNumber from
